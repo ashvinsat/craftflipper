0a5b58e739ee0ee62b9f203dfbdf4b0b5522bbb2
ashvinsat <88851088+ashvinsat@users.noreply.github.com>
2023-07-03 18:39:19 +0000

    new file:   apistuff.py 	new file:   sampleitem.json

---
+++ apistuff.py
@@ -0,0 +1,14 @@
+import requests
+
+"""
+cofl stuff:
+https://sky.coflnet.com/api/auctions/tag/NAME/active/bin
+startingBid:
+
+bz prices:
+https://sky.shiiyu.moe/api/v2/bazaar
+
+
+
+"""
+bz = requests.get("https://sky.shiiyu.moe/api/v2/bazaar")
