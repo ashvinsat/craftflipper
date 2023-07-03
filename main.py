549cca69b92f4455333ffedc2c7d5d57837d8e25
ashvinsat <88851088+ashvinsat@users.noreply.github.com>
2023-07-03 17:39:44 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-import requests
+#import os
+import tracemalloc
+import discord
+from discord.ext import commands, tasks
+import configparser as cfg
+tracemalloc.start()
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="~", intents= intents, case_insensitive=True)
 
+parser = cfg.RawConfigParser()
+FILE = "config.ini"
+parser.read(FILE)
+token=parser.get("Info", "TOKEN")
+bot.run(token)
