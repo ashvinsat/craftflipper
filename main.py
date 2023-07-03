8946c5647b04e0b869ad5d305d64a5a9e65d6ff3
ashvinsat <88851088+ashvinsat@users.noreply.github.com>
2023-07-03 18:02:42 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # pylint: disable=consider-using-f-string
 # pylint: disable=trailing-newlines
 # pylint: disable=unused-import
+# pylint: disable=missing-function-docstring
 # pylint: disable=missing-module-docstring
 import os
 import tracemalloc
@@ -8,17 +9,17 @@ import configparser as cfg
 import discord
 from discord.ext import commands, tasks
 tracemalloc.start()
+#bot stuff
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="~", intents= intents, case_insensitive=True)
-
 parser = cfg.RawConfigParser()
 FILE = "config.ini"
 parser.read(FILE)
 token=parser.get("Info", "TOKEN")
-bot.run(token)
 
 @bot.command()
 async def echo(ctx, arg):
     await ctx.send(arg)
 
+bot.run(token)
